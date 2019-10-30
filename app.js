b730d48195fe5d347700e2d15e2d3b90016da0d6
chenyue <chenyue@yunzongnet.com>
2019-10-30 19:00:49 +0800

    开发基础功能0.0.2

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express()
 
+const router = require('./router')
+
 app.use(express.static('./public'))
 
-app.use(9090, (req, res) => {
+app.use('/route', router)
+
+app.use('text', (req, res) => {
   res.send({'state': true})
 })
 
